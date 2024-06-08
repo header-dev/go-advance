b3a56cdae3c329f08b128fac36fc7b3e0e984f8c
Wit Tech <kritawit.bunket@aia.com>
2024-06-08 23:02:09 +0700

    Completed Goroutine with Select statement for channel

---
+++ forEach/go.mod
@@ -0,0 +1,3 @@
+module header-dev/forEach
+
+go 1.22.0
---
+++ forEach/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+	qCh := make(chan struct{})
+
+	go fibonacci(ch, qCh)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
+
+	qCh <- struct{}{}
+
+	<-qCh
+	fmt.Println("End")
+
+}
+
+func fibonacci(ch chan int, qCh chan struct{}) {
+	a, b := 0, 1
+
+	for {
+		select {
+		case ch <- a:
+			a, b = b, a+b
+		case <-qCh:
+			qCh <- struct{}{}
+			return
+		}
+
+	}
+}
